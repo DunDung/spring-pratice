0b670518549cdc75f8b96eaa370b351fc79d5e9e
JunSeong <ebseud6135@naver.com>
2020-12-09 11:33:20 +0900

    feat: 스프링 MVC를 이용한 게시판 구축 프로젝트 세팅
       - 스프링 입문을 위한 자바 객체 지향의 원리와 이해 마지막 장, 게시판 예제

---
+++ frog-book-spring-mvc-board/src/main/java/com/example/frogbookspringmvcboard/FrogBookSpringMvcBoardApplication.java
@@ -0,0 +1,13 @@
+package com.example.frogbookspringmvcboard;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FrogBookSpringMvcBoardApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(FrogBookSpringMvcBoardApplication.class, args);
+    }
+
+}
---
+++ frog-book-spring-mvc-board/src/test/java/com/example/frogbookspringmvcboard/FrogBookSpringMvcBoardApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.frogbookspringmvcboard;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FrogBookSpringMvcBoardApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
